55cf06787dcd84b12fdd45777a0415de90c5c714
AkeabMesfin <akeabmesfin@gmail.com>
2024-12-15 15:59:50 +0300

    Delete chat directory

--- chat/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import GroupChat, PrivateChat, PrivateMessage, GroupMessage
-
-admin.site.register(GroupChat)
-admin.site.register(PrivateChat)
-admin.site.register(PrivateMessage)
-admin.site.register(GroupMessage)
