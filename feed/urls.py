3be41d634fb59c29c906218cc7adab0d1693de45
AkeabMesfin <akeabmesfin@gmail.com>
2024-12-15 16:01:15 +0300

    Delete feed directory

--- feed/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from . import views
-urlpatterns = [
-    path('feed', views.feed, name='feed'),
-    path('feed/like/<int:post_id>', views.like, name='like'),
-    path('feed/bookmark/<int:post_id>', views.bookmark, name='bookmark'),
-    path('feed/comments/<int:post_id>/', views.comments, name='comments'),
-    path('feed/create-post', views.create_post, name='create-post'),
-]
